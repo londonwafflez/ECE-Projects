47ed7cd1cb211c0224dd2b94acc903369dca5c46
Zachary Miller <93176330+londonwafflez@users.noreply.github.com>
2026-04-20 23:38:21 -0400

    Pass modExp test

--- lab_runtime/student_crypto.cpp
+++ lab_runtime/student_crypto.cpp
@@ -18,28 +18,37 @@ namespace {
 // BEGIN STUDENT CODE
 
 // int main() {
-//     // cout << encryptCaesar("abc xy35./z", -28) << endl;
-//     // cout << decryptCaesar(encryptCaesar("abc xy35./z", -5), -5) << endl;
-//     // for (uint32_t i : encryptRSA("hi", 3, 391)) {printf("%d, ", i);}; 
-//     for (uint8_t i : encryptLFSR("abc", 0xA7)) {printf("%X, ", i);}; 
-//     cout << endl;
-//     for (uint8_t i : decryptLFSR(vector<uint8_t>{0xC6, 0x96, 0xED}, 0xA7)) {printf("%c, ", i);}; 
-//     cout << endl;
-//     // cout << ("Decrypt RSA: " + decryptRSA(vector<uint32_t>{348, 265}, 3, 391)) << endl; 
-//     // printf("Step LFSR %x\n", stepLFSR(0xA7)); // 0x53
-//     // 1010 0111 -> 0101 0011
-//     //   A7      -> 53
-//     // d3 = 1101 0011
+    // printf("Modexp: %d", modExp(79,17,3233));
+
+    // cout << encryptCaesar("abc xy35./z", -28) << endl;
+    // cout << decryptCaesar(encryptCaesar("abc xy35./z", -5), -5) << endl;
+    // for (uint32_t i : encryptRSA("hi", 3, 391)) {printf("%d, ", i);}; 
+    // for (uint8_t i : encryptLFSR("abc", 0xA7)) {printf("%X, ", i);}; 
+    // cout << endl;
+    // for (uint8_t i : decryptLFSR(vector<uint8_t>{0xC6, 0x96, 0xED}, 0xA7)) {printf("%c, ", i);}; 
+    // cout << endl;
+    // cout << ("Decrypt RSA: " + decryptRSA(vector<uint32_t>{348, 265}, 3, 391)) << endl; 
+    // printf("Step LFSR %x\n", stepLFSR(0xA7)); // 0x53
+    // 1010 0111 -> 0101 0011
+    //   A7      -> 53
+    // d3 = 1101 0011
+    // if (0x11 & 16) cout << endl;
     
 //     return 0;
 // }
 
 uint32_t modExp(uint32_t base, uint32_t exp, uint32_t mod) {
-    // uint16_t one_e = exp & 0xF0;
-    // uint16_t two_e = exp & 0x0F;
-    uint32_t out = pow(base, exp);
-    // cout << (mod == 0 ? 0u : out % mod) << endl;
-    return mod == 0 ? 0u : out % mod;
+    uint64_t out = 1;
+    if (exp & 1) out = base;
+    cout << out << endl;
+    for (int i = 1; i < 32; i++) {
+        if (exp & static_cast<uint32_t>(pow(2, i))) {
+            for (int j = 0; j < pow(2, i); j++) {
+                out = base % mod * out %  mod;
+            }
+        }
+    }
+    return out;
 }
 
 uint8_t stepLFSR(uint8_t state) {
